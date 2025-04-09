a49bae2735a62121e7e7c2e1b7085f40fef599c3
haideralimalikk <haideralimalik3008@gmail.com>
2025-04-09 16:28:44 +0000

    Updated Server.js for pipeline 2.0

--- server.js
+++ server.js
@@ -13,9 +13,16 @@ var handleRequest = function(request, response) {
   console.log("Running On:", host, "| Total Requests:", ++requests, "| App Uptime:", (new Date() - startTime) / 1000, "seconds", "| Log Time:", new Date());
 }
 
+// Set a timeout to stop the server after a specified time (e.g., 60 seconds)
+setTimeout(function() {
+  console.log('Shutting down server after timeout');
+  process.exit(0);  // Exit the application gracefully after 60 seconds
+}, 60000); // 60000 ms = 60 seconds
+
 var www = http.createServer(handleRequest);
 www.listen(8081, function () {
   startTime = new Date();
   host = process.env.HOSTNAME;
   console.log("Started At:", startTime, "| Running On:", host, "\n");
 });
+
