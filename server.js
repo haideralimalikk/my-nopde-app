a6d8fca334fafb367337bd6d3aadc4dc8db29abb
haideralimalikk <haideralimalik3008@gmail.com>
2025-04-09 16:47:56 +0000

    Updated Server.js for pipeline increase Timeout

--- server.js
+++ server.js
@@ -13,12 +13,7 @@ var handleRequest = function(request, response) {
   console.log("Running On:", host, "| Total Requests:", ++requests, "| App Uptime:", (new Date() - startTime) / 1000, "seconds", "| Log Time:", new Date());
 }
 
-// Set a timeout to stop the server after a specified time (e.g., 60 seconds)
-setTimeout(function() {
-  console.log('Shutting down server after timeout');
-  process.exit(0);  // Exit the application gracefully after 60 seconds
-}, 60000); // 60000 ms = 60 seconds
-
+// Removed the timeout for server shutdown
 var www = http.createServer(handleRequest);
 www.listen(8081, function () {
   startTime = new Date();
