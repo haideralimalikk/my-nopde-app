e76af565283d1ae879e9212209c93c2bf1f2e3f6
haideralimalikk <haideralimalik3008@gmail.com>
2025-04-20 23:28:14 +0000

    random commit for video demo 3.0

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
-var http = require('http');
-var requests = 0;
-var startTime;
-var host = require('os').hostname();  // Use os.hostname() directly to get the system's hostname
-//haider ali commit
-var handleRequest = function(request, response) {
-  response.setHeader('Content-Type', 'text/plain');
-  response.writeHead(200);
-  response.write("DevOps Coursework 2! | Running on: ");
-  response.write(host);
-  response.end(" | v=0\n");
-  console.log("Running On:", host, "| Total Requests:", ++requests, "| App Uptime:", (new Date() - startTime) / 1000, "seconds", "| Log Time:", new Date());
-}
 const http = require('http');
 const hostname = require('os').hostname();
-const port = process.env.PORT || 8081; // Use environment variable
+const port = process.env.PORT || 8081;
 
 let requests = 0;
 let startTime = new Date();
+
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.writeHead(200);
